fe4740ab606f7cfa3e5319ab6715c3a366274f47
trinamae12 <maetrina12@gmail.com>
2019-07-29 23:57:00 +0800

    Initial commit

---
+++ src/app/product-details/product-details.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+
+import {products} from '../products';
+import{CartService} from '../cart.service';
+
+@Component({
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.css']
+})
+export class ProductDetailsComponent implements OnInit {
+  product;
+
+  constructor(
+    private route: ActivatedRoute,
+    private cartService: CartService,
+  ) {}
+
+  addProductToCart(product){
+    window.alert("Your product has been added to cart!");
+    this.cartService.addToCart(product);
+  }
+
+  ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      this.product = products[+params.get('productId')]
+    });
+  }
+
+}
